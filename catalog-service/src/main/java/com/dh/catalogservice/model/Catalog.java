590e5be3e8b9f36f22900d29c2f8f1bf2c51760c
ATashko <tashkomendez@gmail.com>
2023-09-02 20:29:51 -0500

    adding files from base project

---
+++ catalog-service/src/main/java/com/dh/catalogservice/model/Catalog.java
@@ -0,0 +1,27 @@
+package com.dh.catalogservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Catalog implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String genre;
+
+}
